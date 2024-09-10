ce25ade570142f505f46d51a2613b2599f844d2b
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-09-11 02:37:59 +0900

    use iframe

--- page/ws/src/app/page.tsx
+++ page/ws/src/app/page.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Document, Page } from 'react-pdf';
 
 // Components
 import { About } from '@/component/page/about';
@@ -14,7 +13,7 @@ function usePDFs() {
 
   useEffect(() => {
     const fetchPDFs = async () => {
-      const response = await fetch(`/${__PROJECT_BASENAME__}/files.txt`);
+      const response = await fetch(`/${__PROJECT_BASENAME__}/test.txt`);
       const content = await response.text();
       const pdfs_list = content.split('\n');
       const pdfs = pdfs_list.reduce((acc, pdf) => {
@@ -37,6 +36,7 @@ export function MainPage() {
   const [selected_pdf, setSelectedPDF] = useState<string | null>(null);
 
   const handlePDFClick = (pdf: string) => {
+    console.log(`set pdf: ${pdf}`);
     setSelectedPDF(pdf);
   };
 
@@ -44,12 +44,17 @@ export function MainPage() {
     <>
       <Typography variant="h4">Beamer Samples</Typography>
 
-      <Box display="flex" height="80vh">
+      <Box display="flex" height="80vh" width={'100%'}>
         {/* ボタンを縦に並べるエリア */}
         <Box maxHeight={'80vh'} p={2} sx={{ overflowY: 'auto' }}>
           <Stack spacing={2}>
             {Object.entries(pdfs).map(([theme_name, pdf], index) => (
-              <Button key={index} variant="contained" onClick={() => handlePDFClick(pdf)}>
+              <Button
+                key={index}
+                sx={{ textTransform: 'none' }}
+                variant="contained"
+                onClick={() => handlePDFClick(pdf)}
+              >
                 {theme_name}
               </Button>
             ))}
@@ -59,9 +64,15 @@ export function MainPage() {
         {/* PDF表示エリア */}
         <Box flexGrow={1} p={2}>
           {selected_pdf ? (
-            <Document file={`/${__PROJECT_BASENAME__}/${selected_pdf}`}>
-              <Page pageNumber={1} />
-            </Document>
+            <Box width={'100%'} height={'100%'} border={'1px solid'}>
+              <iframe
+                src={`/${__PROJECT_BASENAME__}/${selected_pdf}`}
+                width={'100%'}
+                height={'100%'}
+                style={{ border: 'none' }}
+                title={selected_pdf}
+              ></iframe>
+            </Box>
           ) : (
             <Box display="flex" justifyContent="center" alignItems="center" height="100%">
               <p>PDFファイルを選択してください</p>
